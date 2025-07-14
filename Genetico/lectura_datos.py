6c3539c0b34533a20b0f37a89caf6fd8e2c20ce5
Lucas Becerra <lucas.becerra@ib.edu.ar>
2025-07-14 18:09:18 -0300

    agrego area quemada y cambio calculo del viento

--- Genetico/lectura_datos.py
+++ Genetico/lectura_datos.py
@@ -8,7 +8,7 @@ from operadores_geneticos import poblacion_inicial
 
 def leer_asc(ruta):
     with open(ruta, 'r') as f:
-        for i in range(6):  
+        for _ in range(6):  
             f.readline()
         data = np.loadtxt(f) 
         return cp.array(data, dtype=cp.float32) 
@@ -17,30 +17,33 @@ def leer_asc(ruta):
 
 def preprocesar_datos():
     datos = [leer_asc(m) for m in ruta_mapas]
-    vientod, vientov, pendiente, vegetacion, orientacion = datos
+    vientod, vientov, pendiente, vegetacion, orientacion, area_quemada = datos
 
     # Parámetros derivados
     beta_veg = cp.where(vegetacion <= 2, 0, 0.1 * vegetacion)
     gamma = cp.where(vegetacion <= 2, 100, 0.1)
 
-    wx = vientov * cp.cos(5/2 * cp.pi - vientod * cp.pi / 180) * 1000
-    wy = - vientov * cp.sin(5/2 * cp.pi - vientod * cp.pi / 180) * 1000
+    vientod_rad = vientod * cp.pi / 180
+    # Componentes cartesianas del viento:
+    wx = -vientov * cp.sin(vientod_rad) * 1000  # Este = sin(ángulo desde Norte)
+    wy = -vientov * cp.cos(vientod_rad) * 1000 # Norte = cos(ángulo desde Norte)
 
     h_dx = cp.tan(pendiente * cp.pi / 180) * cp.cos(orientacion * cp.pi / 180 - cp.pi/2)
     h_dy = cp.tan(pendiente * cp.pi / 180) * cp.sin(orientacion * cp.pi / 180 - cp.pi/2)
 
     return {
-        "vientod": vientod,
-        "vientov": vientov,
-        "pendiente": pendiente,
-        "vegetacion": vegetacion,
-        "orientacion": orientacion,
-        "beta_veg": beta_veg,
-        "gamma": gamma,
-        "wx": wx,
-        "wy": wy,
-        "h_dx": h_dx,
-        "h_dy": h_dy,
+        "vientod": cp.flipud(vientod),
+        "vientov": cp.flipud(vientov),
+        "pendiente": cp.flipud(pendiente),
+        "vegetacion": cp.flipud(vegetacion),
+        "orientacion": cp.flipud(orientacion),
+        "beta_veg": cp.flipud(beta_veg),
+        "gamma": cp.flipud(gamma),
+        "wx": cp.flipud(wx),
+        "wy": cp.flipud(wy),
+        "h_dx": cp.flipud(h_dx),
+        "h_dy": cp.flipud(h_dy),
+        "area_quemada": cp.flipud(area_quemada),
         "ny": vientod.shape[0],
         "nx": vientod.shape[1],
     }
