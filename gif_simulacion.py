884b4965d92a643b0c251624e215292410610e5f
Lucas Becerra <lucas.becerra@ib.edu.ar>
2025-11-04 18:42:35 -0300

    .

--- gif_simulacion.py
+++ gif_simulacion.py
@@ -5,13 +5,60 @@ import cupyx.scipy.ndimage #type: ignore
 import matplotlib.pyplot as plt
 import imageio
 import os
-from Genetico.lectura_datos import preprocesar_datos
+from Genetico.config import ruta_mapas
 
 ############################## FUNCIÓN PARA AGREGAR UNA DIMENSIÓN ###############################################
 
 def create_batch(array_base, n_batch):
     # Se repite array_base n_batch veces en un bloque contiguo
     return cp.tile(array_base[cp.newaxis, :, :], (n_batch, 1, 1)).copy()
+
+############################## LEER MAPAS RASTER ###############################################
+
+def leer_asc(ruta):
+    with open(ruta, 'r') as f:
+        for _ in range(6):  
+            f.readline()
+        data = np.loadtxt(f) 
+        return cp.array(data, dtype=cp.float32) 
+
+############################## CALCULO A PARTIR DE MAPAS ###############################################
+
+def preprocesar_datos():
+    datos = [leer_asc(m) for m in ruta_mapas]
+    vientod, vientov, pendiente, vegetacion, orientacion = datos
+
+    # Parámetros derivados
+    beta_veg = cp.where(vegetacion <= 2, 0, 0.1 * vegetacion)
+    gamma = cp.where(vegetacion <= 2, 100, 0.1)
+
+    vientod_rad = vientod * cp.pi / 180
+    # Componentes cartesianas del viento:
+    wx = -vientov * cp.sin(vientod_rad) * 1000  # Este = sin(ángulo desde Norte)
+    wy = -vientov * cp.cos(vientod_rad) * 1000 # Norte = cos(ángulo desde Norte)
+
+    slope = pendiente / 100.0 # pendiente en porcentaje
+    orientacion_rad = orientacion * cp.pi / 180
+
+    h_dx = -slope * cp.sin(orientacion_rad)
+    h_dy = -slope * cp.cos(orientacion_rad)
+
+    return {
+        "vientod": cp.flipud(vientod),
+        "vientov": cp.flipud(vientov),
+        "pendiente": cp.flipud(pendiente),
+        "vegetacion": cp.flipud(vegetacion),
+        "orientacion": cp.flipud(orientacion),
+        "beta_veg": cp.flipud(beta_veg),
+        "gamma": cp.flipud(gamma),
+        "wx": cp.flipud(wx),
+        "wy": cp.flipud(wy),
+        "h_dx": cp.flipud(h_dx),
+        "h_dy": cp.flipud(h_dy),
+        "ny": vientod.shape[0],
+        "nx": vientod.shape[1],
+    }
+
 
 ############################## CARGADO DE MAPAS ###############################################
 
@@ -118,7 +165,7 @@ I_batch[:, y_ignicion, x_ignicion] = 1
 # var_poblacion = 0
 
 # Inicializar arrays de cupy para almacenar los resultados
-num_steps = 500
+num_steps = 2000
 # pob_total = cp.zeros(num_steps)
 # S_total = cp.zeros(num_steps)
 # I_total = cp.zeros(num_steps)
