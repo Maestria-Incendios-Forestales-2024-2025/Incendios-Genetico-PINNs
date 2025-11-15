a4255fe4bcb5e12bf2b80747f89ebe5c15ee78a4
Lucas Becerra <lucas.becerra@ib.edu.ar>
2025-11-15 11:23:44 -0300

    modifico fuerza_bruta para que corra con los 3 experimentos sintéticos

--- Genetico/fuerza_bruta.py
+++ Genetico/fuerza_bruta.py
@@ -1,39 +1,121 @@
 import cupy as cp # type: ignore
 import csv, os
-from lectura_datos import preprocesar_datos
+from lectura_datos import preprocesar_datos, guardar_resultados
 from operadores_geneticos import poblacion_inicial
 from algoritmo import procesar_poblacion_batch
+from config import cota, d, dt
+import socket, argparse
+
+print(cp.cuda.runtime.getDeviceProperties(0)['name'])
+
+# Obtengo la variable por línea de comando
+parser = argparse.ArgumentParser()
+parser.add_argument("--exp", type=int, default=1, help="Número de experimento")
+args = parser.parse_args()
+
+exp = args.exp
+
+############################## CARGADO DE MAPAS #######################################################
 
 datos = preprocesar_datos()
-
-vegetacion = datos["vegetacion"]
 wx = datos["wx"]
 wy = datos["wy"]
 h_dx_mapa = datos["h_dx"]
 h_dy_mapa = datos["h_dy"]
 
-############################## PARÁMETROS DE LOS MAPAS ###############################################
+############################## INCENDIO DE REFERENCIA ####################################################
 
-d = 30 # Tamaño de cada celda
-beta_veg = cp.where(vegetacion <= 2, 0, 0.1 * vegetacion) # Parámetros del modelo SI
-gamma = cp.where(vegetacion <= 2, 0, 0.1) # Hacemos una máscara donde vegetación <=2, gamma >> 1/dt. Sino, vale 0.1. 
-dt = 1/6 # Paso temporal.
+# Detecto dónde estoy corriendo
+hostname = socket.gethostname()
+print(hostname)
 
-############################## CONDICIÓN DE COURANT PARA LOS TÉRMINOS DIFUSIVOS Y ADVECTIVOS ###############################################
+if "rocks7frontend" in hostname or "compute" in hostname:
+    base_cluster = "/home/lucas.becerra/Incendios-Genetico-PINNs/"
+    rutas = {
+        1: base_cluster + "R_referencia_1.npy",
+        2: base_cluster + "R_referencia_2.npy",
+        3: base_cluster + "R_referencia_3.npy",
+    }
+elif "ccad.unc.edu.ar" in hostname:
+    base_ccad = "/home/lbecerra/Incendios-Genetico-PINNs/"
+    rutas = {
+        1: base_ccad + "R_referencia_1.npy",
+        2: base_ccad + "R_referencia_2.npy",
+        3: base_ccad + "R_referencia_3.npy",
+    }
+else:
+    base_local = "c:/Users/becer/OneDrive/Desktop/Maestría en Ciencias Físicas/Tesis/Incendios-Forestales---MCF-2024-2025/"
+    rutas = {
+        1: base_local + "R_referencia_1.npy",
+        2: base_local + "R_referencia_2.npy",
+        3: base_local + "R_referencia_3.npy",
+    }
 
-D_max = d**2 / (2*dt)
-A_max = d / (cp.sqrt(2)*dt*cp.max(cp.sqrt(wx**2+wy**2)))
-B_max = d / (cp.sqrt(2)*dt*cp.max(cp.sqrt(h_dx_mapa**2+h_dy_mapa**2)))
+# Selecciono la ruta según EXP
+ruta_incendio_referencia = rutas[exp]
+print(f"Leyendo mapa de incendio de referencia: {os.path.basename(ruta_incendio_referencia)}")
+
+############################## CONDICIÓN DE COURANT PARA LOS TÉRMINOS DIFUSIVOS Y ADVECTIVOS ####################################
+
+A_max = float(d / (cp.sqrt(2)*dt/2*cp.max(cp.sqrt(wx**2+wy**2)))) # constante de viento
+B_max = float(d / (cp.sqrt(2)*dt/2*cp.max(cp.sqrt(h_dx_mapa**2+h_dy_mapa**2)))) # constante de pendiente
+
+############################## DISEÑO DE EXPERIMENTOS ##########################################
+
+limite_parametros_base = [
+    (0.01, 100.0),          # D
+    (0.0, A_max * cota),    # A
+    (0.0, B_max * cota)     # B
+]
+
+print(f"Corriendo el experimento {exp}")
+
+if exp == 1:
+    ajustar_beta_gamma = False
+    ajustar_ignicion = True
+
+    limite_ignicion = [(300, 720), (400, 800)]
+    limite_parametros = limite_parametros_base + limite_ignicion
+
+    beta_fijo = [0.91, 0.72, 1.38, 1.94, 0.75]
+    gamma_fijo = [0.5, 0.38, 0.84, 0.45, 0.14]
+
+    ignicion_fija_x = None
+    ignicion_fija_y = None 
+
+elif exp == 2:
+    ajustar_beta_gamma = True
+    ajustar_ignicion = True
+
+    limite_ignicion = [(300, 720), (400, 800)]
+    limite_beta = [(0.1, 2.0)]
+    limite_gamma = [(0.1, 0.9)]
+    limite_parametros = limite_parametros_base + limite_ignicion + limite_beta + limite_gamma
+
+elif exp == 3:
+    ajustar_beta_gamma = True
+    ajustar_ignicion = False
+
+    limite_beta = [(0.1, 2.0)] * 5
+    limite_gamma = [(0.1, 0.9)] * 5
+    limite_parametros = limite_parametros_base + limite_beta + limite_gamma
+
+    ignicion_fija_x = [1130, 1300, 620]
+    ignicion_fija_y = [290, 150, 280]
+
+    ignicion_fija_x = None
+    ignicion_fija_y = None
+
+else:
+    raise ValueError(f"Experimento {exp} no está definido")
 
 ############################## PARÁMETROS DE LOS INCENDIOS SIMULADOS ###############################################
 
 # Total de simulaciones y tamaño de bloque
-num_total_simulaciones = 20
-tamano_bloque = 20
-batch_size = 20
+num_total_simulaciones = 100000
+tamano_bloque = 10000
+batch_size = 2
 
-cota = 0.95
-limite_parametros = [(0, 200), (0, A_max * cota), (0, B_max * cota), (500, 900), (500, 900)]
 combinaciones = poblacion_inicial(num_total_simulaciones, limite_parametros) 
 
 ############################## BARRIDA DE PARÁMETROS ###############################################
@@ -43,13 +125,17 @@ end = cp.cuda.Event()
 
 start.record()
 
-num_steps = 1000
-ruta_incendio_referencia = 'Genetico/R_final.npy'
+num_steps = 500
 
-resultados_dir = f'resultados_fuerza_bruta'
+if "rocks7frontend" in hostname or "compute" in hostname:
+    job_id = os.environ.get('JOB_ID', 'default')
+elif "ccad.unc.edu.ar" in hostname:
+    job_id = os.environ.get("SLURM_JOB_ID", None)
+else:
+    job_id = 'default'
+        
+resultados_dir = f'resultados/fuerza_bruta_task_{job_id}'
 os.makedirs(resultados_dir, exist_ok=True)
-# Obtener el task_id del SGE
-task_id = os.environ.get('JOB_ID', 'default')
 
 # Loop por bloques
 for i in range(0, num_total_simulaciones, tamano_bloque):
@@ -68,21 +154,32 @@ for i in range(0, num_total_simulaciones, tamano_bloque):
         ruta_incendio_referencia=ruta_incendio_referencia,
         limite_parametros=limite_parametros,
         num_steps=num_steps,
-        batch_size=batch_size
+        batch_size=batch_size,
+        ajustar_beta_gamma=ajustar_beta_gamma,
+        beta_fijo=beta_fijo,
+        gamma_fijo=gamma_fijo,
+        ajustar_ignicion=ajustar_ignicion,
+        ignicion_fija_x=ignicion_fija_x,
+        ignicion_fija_y=ignicion_fija_y
     )
 
     # Guardar resultados de este bloque
     bloque_id = inicio // tamano_bloque
-    csv_filename = os.path.join(resultados_dir, f'resultados_bloque_{bloque_id}_task_{task_id}.csv')
-    with open(csv_filename, 'w', newline='') as csvfile:
-        fieldnames = ['D', 'A', 'B', 'x', 'y', 'fitness']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for resultado in resultados:
-            writer.writerow(resultado)
+
+    # Intentar convertir a entero
+    try:
+        job_id = int(job_id)
+    except (TypeError, ValueError):
+        job_id = 0  # o algún valor por defecto numérico
+
+    num_archivo = job_id + bloque_id
+
+    guardar_resultados(resultados, resultados_dir, num_archivo, 
+                       ajustar_beta_gamma=ajustar_beta_gamma,
+                       ajustar_ignicion=ajustar_ignicion)
 
     print(f'Resultados guardados en: {resultados_dir}')
-    print(f'Task ID: {task_id}')
+    print(f'Task ID: {job_id}')
 
 end.record()  # Marca el final en GPU
 end.synchronize() # Sincroniza y mide el tiempo
